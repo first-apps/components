149d00544e1dabaa1b9a45e589b57dc6e574d0c6
dikshit-n <62920150+dikshit-n@users.noreply.github.com>
2024-12-14 22:50:06 +0530

    package rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import reportWebVitals from "./reportWebVitals";
 
 // do not import any css files in this tsx file
 // the webpack is configured to only process the css inside the src/app and src/lib folder
@@ -15,8 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
